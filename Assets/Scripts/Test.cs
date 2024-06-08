65a8a13e486f3e654e2796b1f5d7d3144a05b60f
CLilRain <JacksonKing.CL@gmail.com>
2024-06-08 13:19:29 +1000

    Update Test.cs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -6,6 +6,9 @@ public class Test : MonoBehaviour
 {
 <<<<<<< Updated upstream
     // I am B
+=======
+    // I am A
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
