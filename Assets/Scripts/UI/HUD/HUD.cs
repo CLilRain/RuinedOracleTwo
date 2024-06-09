2c2c2b2d879f2ce6210213efb81f9d0747f550aa
CLilRain <JacksonKing.CL@gmail.com>
2024-06-09 12:35:06 +1000

    HUD meter and end turn buttons

--- Assets/Scripts/UI/HUD/HUD.cs
+++ Assets/Scripts/UI/HUD/HUD.cs
@@ -1,21 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class HUD : MonoBehaviour
 {
     [SerializeField]
-    protected 
+    protected TMP_Text timePointText;
 
+    [SerializeField]
+    protected TMP_Text essenceText;
 
+    [SerializeField]
+    protected Image timePointBar;
 
-    void Start()
+    [SerializeField]
+    protected Image essenceBar;
+
+    protected Turn_UI gameplayUI;
+
+#if UNITY_EDITOR
+    protected float currrentTime = 60;
+    protected float maxTime = 60;
+
+    protected int currentEssence = 6;
+    protected int maxEssence = 6;
+#endif
+
+    protected virtual void Start()
     {
-        
+        gameplayUI = FindObjectOfType<Turn_UI>();
+
+#if UNITY_EDITOR
+        SetEssence(currentEssence, maxEssence);
+#endif
     }
 
-    void Update()
+    protected virtual void Update()
     {
-        
+#if UNITY_EDITOR
+        currrentTime -= Time.deltaTime;
+        SetTimePoints(Mathf.Round(currrentTime), Mathf.Round(maxTime));
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            currentEssence--;
+            SetEssence(currentEssence, maxEssence);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            currentEssence++;
+            SetEssence(currentEssence, maxEssence);
+        }
+#endif
+    }
+
+    public virtual void SetTimePoints(int current, int max)
+    {
+        SetTimePoints(current, max);
+    }
+
+    public virtual void SetTimePoints(float current, float max)
+    {
+        timePointBar.fillAmount = current / max;
+        timePointText.text = string.Format("{0} sec", current);
+    }
+
+    public virtual void SetEssence(int current, int max)
+    {
+        essenceBar.fillAmount = (float)current / (float)max;
+        essenceText.text = string.Format("{0} / {1}", current, max);
+    }
+
+    public virtual void ClickedEndTurn()
+    {
+        if (!gameplayUI)
+        {
+            Debug.Log("Turn_UI (Max's class) is not connected.");
+        }
+
+        gameplayUI.onPressedEndTurn();
     }
 }
